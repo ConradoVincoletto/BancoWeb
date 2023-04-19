4d02aabb989dd499c2978262cca4e2b934b13593
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-04-19 19:00:40 -0300

    feat: pasta view Models e classe Login

---
+++ BancoWeb/Models/ViewMoldes/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BancoWeb.Models.ViewMoldes
+{
+    public class Login
+    {
+        [Required]
+        public int Numero { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public int Senha { get; set; }
+
+
+    }
+}
